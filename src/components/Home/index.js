8b3eb0d8656b44dcfe2029df3b9cec6a1019d271
Zahid Abdullah <zaevanphy@gmail.com>
2023-03-22 12:04:48 +0200

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,8 +6,28 @@ export default function Home() {
     <div className="container-home">
       <img src={HomeImage} alt="home" />
       <div className="text-field">
-        <h1>Welcome to my website</h1>
-        <p>Learn more about me and my work.</p>
+        <h1>Welcome to Parturi KKN</h1>
+        <p>
+          Your go-to barber shop in Joensuu, Finland! Our team of experienced
+          and skilled barbers is dedicated to providing our clients with
+          top-notch grooming services in a warm and inviting atmosphere. At
+          Parturi KKN, we understand that every client is unique, and we take
+          the time to listen to your needs and preferences to ensure that you
+          leave our shop feeling confident and satisfied with your new look.
+          Whether you're looking for a classic haircut or a modern style, we've
+          got you covered. Our barbers are passionate about their craft and stay
+          up-to-date with the latest trends and techniques in men's grooming. We
+          use only the highest quality products and tools to ensure that you get
+          the best possible results. In addition to haircuts, we offer a range
+          of other services to keep you looking and feeling your best, including
+          beard trims, shaves, and facial treatments. We also offer grooming
+          tips and advice to help you maintain your look in between visits. At
+          Parturi KKN, we believe that a great haircut is more than just a
+          service – it's an experience. We strive to create a welcoming and
+          comfortable environment where you can relax and enjoy your grooming
+          session. So why wait? Book your appointment today and let us take care
+          of all your grooming needs. We look forward to seeing you soon!
+        </p>
       </div>
     </div>
   );
--- src/components/PriceList/index.js
+++ src/components/PriceList/index.js
@@ -9,40 +9,12 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 export default function PriceList() {
   return (
-    <div>
+    <div className="container-priceList">
       <img src={PricePic} alt="home" />
       <div className="text-priceList">
-        <h1 className="title">Our Prices</h1>
-        {/* your table from here */}
-        <Row xs={1} md={3} className="g-4">
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={hairCard} />
-              <Card.Body>
-                <Card.Title>25 €</Card.Title>
-                <Card.Text>Hair Cut</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={beardCard} />
-              <Card.Body>
-                <Card.Title>20 €</Card.Title>
-                <Card.Text>Beard Trimming</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={studentCard} />
-              <Card.Body>
-                <Card.Title>10% Discount</Card.Title>
-                <Card.Text>Student Price</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <div className="cardPrice"></div>
+        <div className="cardPrice"></div>
+        <div className="cardPrice"></div>
       </div>
     </div>
   );
