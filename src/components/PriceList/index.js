a8971f55e729151db9d092b402ad26dfea0b867a
Zahid Abdullah <zaevanphy@gmail.com>
2023-03-26 17:35:35 +0300

    update

--- src/components/PriceList/index.js
+++ src/components/PriceList/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import "./index.css";
 import PricePic from "../../asset/images/Price.jpg";
-import Card from "react-bootstrap/Card";
 import studentCard from "../../asset/images/studentcard.jpg";
 import hairCard from "../../asset/images/haircard.jpg";
 import beardCard from "../../asset/images/beardcard.jpg";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+
 export default function PriceList() {
   return (
     <div className="container-priceList">
       <img src={PricePic} alt="home" />
       <div className="text-priceList">
-        <div className="cardPrice"></div>
-        <div className="cardPrice"></div>
         <div className="cardPrice"></div>
       </div>
     </div>
