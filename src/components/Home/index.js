253798f3ae822d83418aad189f69b436d6e26ab2
Zahid Abdullah <zahid.abdullah@ecoaims.com>
2023-04-19 13:11:02 +0300

    updatea

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -17,7 +17,7 @@ function Home() {
       <img src={HomeImage} alt="home" />
       <div className="text-field">
       <div className="box">WELCOME TO </div>
-      <div className="box">PARTURI KKN</div>
+      <div className="boxname">PARTURI KKN</div>
         <div className="open-close">
           {isOpen ? (
             <div className="box-open">OPEN</div>
@@ -25,8 +25,17 @@ function Home() {
             <div className="box-close">CLOSED</div>
           )}
         </div>
-        <div className="box">NOW</div>
-        <div> </div>
+        <div>Monday - Friday : 9AM - 7PM</div>
+        <div>Saturday : 9AM - 5PM</div>
+        <div className="box">YOU ARE WELCOME ANYTIME TO OUR SHOP NOW</div>
+        <div className="box">CALL US</div>
+        <div className="boxphoneno"> 046 8456395</div>
+        <div className="box">Get Your Hair Cut With or Without any appointment.</div>
+       
+        <a className="box-open" style={{width:300, backgroundColor:"red"}}
+   href="https://slotti.fi/booking/ESIMERKKI">
+    Book Appointment / Varaa aika
+</a>
       </div>
     </div>
   );
